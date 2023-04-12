f2bd7ec198313d8ff3b858348ea57835b43b4535
Eherny <105952454+Eherny@users.noreply.github.com>
2023-04-11 21:43:08 -0700

    Trying stff

--- client.py
+++ client.py
@@ -1,6 +1,27 @@
+import json
 import socket
 import threading
 import tkinter as tk
+def create_message(report_request_flag=0, report_response_flag=0, join_request_flag=0, join_reject_flag=0,
+                   join_accept_flag=0, new_user_flag=0, quit_request_flag=0, quit_accept_flag=0,
+                   attachment_flag=0, number=0, username='', filename='', payload=''):
+    message = {
+        'REPORT_REQUEST_FLAG': report_request_flag,
+        'REPORT_RESPONSE_FLAG': report_response_flag,
+        'JOIN_REQUEST_FLAG': join_request_flag,
+        'JOIN_REJECT_FLAG': join_reject_flag,
+        'JOIN_ACCEPT_FLAG': join_accept_flag,
+        'NEW_USER_FLAG': new_user_flag,
+        'QUIT_REQUEST_FLAG': quit_request_flag,
+        'QUIT_ACCEPT_FLAG': quit_accept_flag,
+        'ATTACHMENT_FLAG': attachment_flag,
+        'NUMBER': number,
+        'USERNAME': username,
+        'FILENAME': filename,
+        'PAYLOAD_LENGTH': len(payload),
+        'PAYLOAD': payload
+    }
+    return message
 
 class ChatroomClient:
     def __init__(self, host, port):
@@ -8,13 +29,12 @@ class ChatroomClient:
         self.port = port
         self.client_socket = None
         self.username = None
-        self.running = False # flag to indicate if the client is running
+        self.running = False
 
-        # Create the Tkinter GUI
         self.root = tk.Tk()
         self.root.title("Chatroom Client")
         self.root.protocol("WM_DELETE_WINDOW", self.quit)
-        
+
         self.message_listbox = tk.Listbox(self.root, height=20, width=80)
         self.message_listbox.pack(side=tk.TOP, padx=10, pady=10)
 
@@ -30,77 +50,111 @@ class ChatroomClient:
         self.send_button = tk.Button(self.input_frame, text="Send", command=self.send_message)
         self.send_button.pack(side=tk.LEFT)
 
+    def start(self):
+        self.show_menu()
+        self.root.mainloop()
+
+    def send_message(self):
+        message = self.input_entry.get()
+        if message:
+            if message.startswith('/report'):
+                message = create_message(report_request_flag=1, username=self.username)
+        else:
+            message = create_message(payload=message, username=self.username)
+        self.client_socket.send(json.dumps(message).encode())
+        self.input_entry.delete(0, tk.END)
+
+    def get_report(self):
+        self.client_socket.send("/report".encode())
+
+    def join_chatroom(self):
+        self.client_socket.send("/join".encode())
+
+    def show_menu(self):
+        self.create_menu()
+
+    def create_menu(self):
         self.menu_frame = tk.Frame(self.root)
         self.menu_frame.pack(side=tk.BOTTOM, padx=10, pady=10)
 
-        self.report_button = tk.Button(self.menu_frame, text="Get report", command=self.get_report)
-        self.report_button.pack(side=tk.LEFT)
+        self.option_label = tk.Label(self.menu_frame, text="Please select one of the following options:")
+        self.option_label.pack(side=tk.TOP)
 
-        self.join_button = tk.Button(self.menu_frame, text="Join chatroom", command=self.join_chatroom)
-        self.join_button.pack(side=tk.LEFT)
+        self.report_button = tk.Button(self.menu_frame, text="1. Get a report of the chatroom from the server.", command=self.get_report)
+        self.report_button.pack(side=tk.TOP, padx=10, pady=5, anchor='w')
 
-        self.quit_button = tk.Button(self.menu_frame, text="Quit", command=self.quit)
-        self.quit_button.pack(side=tk.LEFT)
+        self.join_button = tk.Button(self.menu_frame, text="2. Request to join the chatroom.", command=self.join_chatroom_and_start)
+        self.join_button.pack(side=tk.TOP, padx=10, pady=5, anchor='w')
 
-    def start(self):
+        self.quit_button = tk.Button(self.menu_frame, text="3. Quit the program.", command=self.quit)
+        self.quit_button.pack(side=tk.TOP, padx=10, pady=5, anchor='w')
+
+
+    def join_chatroom_and_start(self):
         self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         try:
             self.client_socket.connect((self.host, self.port))
         except ConnectionRefusedError:
             print("Could not connect to the chatroom server. Please try again later.")
             return
-        
-        # Create a prompt to enter the username
-        username_label = tk.Label(self.root, text="Enter your username:")
-        username_label.pack(side=tk.TOP, padx=10, pady=10)
+
+        self.prompt_for_username()
+        if self.username is not None:
+            self.client_socket.send(self.username.encode())
+            welcome_message = self.client_socket.recv(1024).decode()
+            self.message_listbox.insert(tk.END, welcome_message)
+            receive_thread = threading.Thread(target=self.receive_messages)
+            receive_thread.start()
+            self.running = True
+            self.join_button.pack_forget()
+
+
+
+    def prompt_for_username(self):
+        self.username_label = tk.Label(self.root, text="Enter your username:")
+        self.username_label.pack(side=tk.TOP, padx=10, pady=10)
 
         self.username_entry = tk.Entry(self.root, width=60)
         self.username_entry.pack(side=tk.TOP)
 
-        username_button = tk.Button(self.root, text="Enter", command=self.set_username)
-        username_button.pack(side=tk.TOP, padx=10, pady=10)
+        self.username_button = tk.Button(self.root, text="Enter", command=self.set_username) # Changed to self.username_button
+        self.username_button.pack(side=tk.TOP, padx=10, pady=10)
 
         self.root.wait_window(self.username_entry)
 
-        # Once the username is entered, send it to the server and display the welcome message
-        self.client_socket.send(self.username.encode())
-        welcome_message = self.client_socket.recv(1024).decode()
-        print(welcome_message)
-        receive_thread = threading.Thread(target=self.receive_messages)
-        receive_thread.start()
-        self.running = True
 
-        # Start the Tkinter event loop
-        self.root.mainloop()
+    def set_username(self):
+        self.username = self.username_entry.get()
+        if self.username:
+            self.username_entry.delete(0, tk.END)
+            self.username_label.pack_forget()
+            self.username_button.pack_forget()
+            self.username_entry.pack_forget()
+            self.root.update()
+        else:
+            self.username_label.config(text="Please enter a non-empty username:")
 
-    def send_message(self):
-        message = self.input_entry.get()
-        if message:
-            self.client_socket.send(message.encode())
-            self.input_entry.delete(0, tk.END)
+
 
     def receive_messages(self):
         while self.running:
             try:
                 message = self.client_socket.recv(1024).decode()
-                self.message_listbox.insert(tk.END, message)
+                message = json.loads(message)
+                payload = message['PAYLOAD']
+                self.message_listbox.insert(tk.END, payload)
             except ConnectionResetError:
-                print("Connection to the server has been lost.")
-                self.running = False
-
-    def get_report(self):
-        self.client_socket.send("/report".encode())
-
-    def join_chatroom(self):
-        self.client_socket.send
+                print("The connection to the server was lost.")
+                break
 
 
     def quit(self):
-        self.client_socket.send("/quit".encode())
-        self.client_socket.close()
+        if self.client_socket:
+            self.client_socket.send("/quit".encode())
+            self.client_socket.close()
         self.running = False
         self.root.destroy()
 
 if __name__ == "__main__":
-    client = ChatroomClient("127.0.0.1", 18000)  # Replace with your chatroom server host and port
+    client = ChatroomClient("127.0.0.1", 18000) # Replace with your chatroom server host and port
     client.start()
--- server.py
+++ server.py
@@ -1,5 +1,26 @@
 import socket
 import threading
+import json
+def create_message(report_request_flag=0, report_response_flag=0, join_request_flag=0, join_reject_flag=0,
+                   join_accept_flag=0, new_user_flag=0, quit_request_flag=0, quit_accept_flag=0,
+                   attachment_flag=0, number=0, username='', filename='', payload=''):
+    message = {
+        'REPORT_REQUEST_FLAG': report_request_flag,
+        'REPORT_RESPONSE_FLAG': report_response_flag,
+        'JOIN_REQUEST_FLAG': join_request_flag,
+        'JOIN_REJECT_FLAG': join_reject_flag,
+        'JOIN_ACCEPT_FLAG': join_accept_flag,
+        'NEW_USER_FLAG': new_user_flag,
+        'QUIT_REQUEST_FLAG': quit_request_flag,
+        'QUIT_ACCEPT_FLAG': quit_accept_flag,
+        'ATTACHMENT_FLAG': attachment_flag,
+        'NUMBER': number,
+        'USERNAME': username,
+        'FILENAME': filename,
+        'PAYLOAD_LENGTH': len(payload),
+        'PAYLOAD': payload
+    }
+    return message
 
 class ChatroomServer:
     def __init__(self):
@@ -35,30 +56,54 @@ class ChatroomServer:
                 print("Connection to the server has been lost.")
                 self.running = False            
     def handle_client(self, client_socket):
-        username = client_socket.recv(1024).decode()
-        if username in self.usernames:
-            client_socket.send("This username is already taken. Please choose another one.".encode())
-            client_socket.close()
-            return
-        self.usernames.append(username)
-        self.clients[client_socket] = username
-        print(f"New connection from {username}")
-        client_socket.send("Welcome to the chatroom!".encode())
-        self.broadcast(f"{username} has joined the chatroom.".encode())
         while True:
-            message = client_socket.recv(1024)
-            if not message:
+            try:
+                raw_message = client_socket.recv(1024)
+                if not raw_message:
+                    break
+                message = json.loads(raw_message.decode())
+            except (ConnectionResetError, json.JSONDecodeError):
                 break
-            self.broadcast(message, username+": ")
-        print(f"{username} has left the chatroom.")
-        self.usernames.remove(username)
-        del self.clients[client_socket]
-        client_socket.close()
-        self.broadcast(f"{username} has left the chatroom.".encode())
+
+            if message["JOIN_REQUEST_FLAG"] == 1:
+                username = message["USERNAME"]
+                if username in self.usernames:
+                    reject_message = create_message(join_reject_flag=1, payload="This username is already taken. Please choose another one.")
+                    client_socket.send(json.dumps(reject_message).encode())
+                    client_socket.close()
+                    return
+                self.usernames.append(username)
+                self.clients[client_socket] = username
+                print(f"New connection from {username}")
+                welcome_message = create_message(join_accept_flag=1, payload="Welcome to the chatroom!")
+                client_socket.send(json.dumps(welcome_message).encode())
+                self.broadcast(json.dumps(create_message(new_user_flag=1, payload=f"{username} has joined the chatroom.")).encode())
+
+            elif message["QUIT_REQUEST_FLAG"] == 1:
+                username = self.clients[client_socket]
+                print(f"{username} has left the chatroom.")
+                self.usernames.remove(username)
+                del self.clients[client_socket]
+                client_socket.close()
+                self.broadcast(json.dumps(create_message(quit_accept_flag=1, payload=f"{username} has left the chatroom.")).encode())
+                break
+
+            elif message["REPORT_REQUEST_FLAG"] == 1:
+                self.send_report(client_socket)
+
+            elif message["PAYLOAD"]:
+                username = self.clients[client_socket]
+                broadcast_message = create_message(payload=f"{username}: {message['PAYLOAD']}")
+                self.broadcast(json.dumps(broadcast_message).encode())
+
 
     def broadcast(self, message, prefix=""):
         for client_socket in self.clients:
             client_socket.send(prefix.encode()+message)
+    def send_report(self, client_socket):
+        num_users = len(self.clients)
+        message = create_message(report_response_flag=1, number=num_users)
+        client_socket.send(json.dumps(message).encode())
 
 if __name__ == "__main__":
     chatroom_server = ChatroomServer()
